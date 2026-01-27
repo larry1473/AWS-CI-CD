aeb54d7e54aef2bd619c62afc40c4e7ce68744db
larry fotso <fotsoguiffo8@gmail.com>
2026-01-26 21:54:54 -0500

    test: delete test

--- test_app.py
+++ test_app.py
@@ -7,7 +7,7 @@ class TestApp(unittest.TestCase):
     def test_say_hello(self):
         self.assertEqual(sayHelleo("World"), "Hello, World!")
         self.assertEqual(sayHelleo("Alice"), "Hello, Alice!")
-        self.assertEqual(sayHelleo("Bob"), "Hello, Bob!")
+        
 
 if __name__ == '__main__':
     unittest.main()
